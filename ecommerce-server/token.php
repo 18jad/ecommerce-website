796954e3a41a92b7abe5dfebfeab6e128c2b7efc
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-20 18:12:24 +0300

    Commented and Cleaned Code

--- ecommerce-server/token.php
+++ ecommerce-server/token.php
@@ -11,6 +11,7 @@ $tokenHeader = json_encode($myObj);
 
 // Functions
 
+//Encodes and Creates the Full Token
 function tokenEncode($header, $payload, $key) {
     $encodedHeader = base64_encode($header);
     $encodedPayload = base64_encode($payload);
@@ -18,17 +19,20 @@ function tokenEncode($header, $payload, $key) {
     return ("$encodedHeader" . "." . "$encodedPayload" . "." . "$encodedSignature");
 };
 
+//Verifies Last Part of Token
 function verifySignature($token, $key) {
     $explodedArray = explode(".", $token);
     $encodedSignature = hash_hmac("sha256", "$explodedArray[0]" . "." . "$explodedArray[1]", $key);
     return ($explodedArray[2] === $encodedSignature);
 };
 
+//Decodes the Payload of the Token
 function tokenDecode($token) {
     $payload = explode(".", $token);
     return base64_decode($payload[1]);
 };
 
+//Verifies Last Part of Token AND Checks if Token Expired
 function tokenAlive($token, $key){
     if(! verifySignature($token, $key)) {
         return false;
@@ -45,7 +49,7 @@ function tokenAlive($token, $key){
     return true;
 };
 
-//Check if user is allowed to manipulate his own data 
+//Check if User is Allowed To Manipulate Data
 function isAuthorized($user, $token, $key) {
     if (! tokenAlive($token, $key)) {
         return false;
