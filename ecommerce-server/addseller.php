621b7676ed59291e6c9746feffe94ee86de75345
unknown <aliiobeid01@gmail.com>
2022-09-21 00:54:52 +0300

    prepare and execute select username query

--- ecommerce-server/addseller.php
+++ ecommerce-server/addseller.php
@@ -1,6 +1,6 @@
 <?php
 
-//declare the headers
+// the headers
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
@@ -13,5 +13,16 @@ $name = $_POST["name"];
 $password = hash("sha256", $_POST["password"]);
 $description = $_POST["description"];
 $money =$_POST["money"];
+$date_joined = date("d M Y @ " . "H" . ":i");
 
+//select the username from the database to check if exist
+$check_seller = $mysql -> prepare(
+    "SELECT username FROM sellers
+    WHERE username = '$username'"
+);
 
+//execute the select query
+$check_seller -> execute();
+$array = $check_seller -> get_result();
+
+$response = [];
