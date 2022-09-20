227d78f08c49ae9821c9865a7811cc30546cc0de
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-21 00:35:36 +0300

    Initialized Variables for Registry API

--- ecommerce-server/client_register.php
+++ ecommerce-server/client_register.php
@@ -1,5 +1,18 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
 include("connection.php");
+
+// Init Variables
+
+$userName = $_POST["userName"];
+$name = $_POST["name"];
+$email = $_POST["email"];
+$dateJoined = date("d M Y @ " . "H" . ":i");
+$password = hash("sha256", $_POST["password"] . $dateJoined . "thcaj5445");
+$money = 0;
+$banned = 0;
 
 ?>
