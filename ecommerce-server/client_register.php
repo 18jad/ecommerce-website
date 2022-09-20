5c93da416914a99b88570090f4c7e1be19ce7bea
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-21 00:51:08 +0300

    Added Check if User Exists Function

--- ecommerce-server/client_register.php
+++ ecommerce-server/client_register.php
@@ -15,4 +15,28 @@ $password = hash("sha256", $_POST["password"] . $dateJoined . "thcaj5445");
 $money = 0;
 $banned = 0;
 
+// Functions
+
+function checkExists($user, $email, $mysql) {
+    $check = $mysql -> prepare(
+        "SELECT username, email FROM users
+        WHERE username = '$user' OR email = '$email'"
+    );
+
+    $check -> execute();
+    $array = $check -> get_result();
+
+    $response = [];
+
+    while($i = $array -> fetch_assoc()){
+        $response[] = $i;
+    };
+
+    if ($response) {
+        die(json_encode("error: username / email already in use."));
+    };
+
+    return true;
+};
+
 ?>
