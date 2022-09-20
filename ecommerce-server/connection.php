29778ef35025100504a0b7923ef434ef91cc6ebc
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-20 18:43:25 +0300

    Commented, Cleaned Code

--- ecommerce-server/connection.php
+++ ecommerce-server/connection.php
@@ -1,10 +1,13 @@
 <?php
 
-$host = "localhost";
-$db_user = "root";
-$db_pass = null;
-$db_name = "ecommerce";
+// Variables
 
+$host = "localhost"; //Where to send data
+$db_user = "root"; //Credentials of sever
+$db_pass = null;
+$db_name = "ecommerce"; //Name of DB
+
+//Create New Instance of Class
 $mysql = new mysqli($host, $db_user, $db_pass, $db_name);
 
 ?>
