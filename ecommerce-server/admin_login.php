d605abe6a1488d7bcfaa1f7b41e253c92a3c5a86
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-21 11:17:19 +0300

    Added Check Password To Admin Login

--- ecommerce-server/admin_login.php
+++ ecommerce-server/admin_login.php
@@ -3,4 +3,40 @@
 include("connection.php");
 include("token.php");
 
+
+
+// Functions
+
+function checkPassword($user, $pass, $mysql) {
+    $hashedPassword = hash("sha256", $pass . "thcaj5445");
+
+    $query = $mysql -> prepare(
+        "SELECT username FROM users
+        WHERE username = '$user' AND password = '$hashedPassword'"
+    );
+
+    $query -> execute();
+    $array = $query -> get_result();
+
+    $response = [];
+    $response[] = $array -> fetch_assoc();
+
+    if($response[0] == null) {
+        die(json_encode("Incorrect Password!"));
+    }
+
+    return true;
+};
+
+// Main
+
+// $dateJoined = retrieveDate($userName, $mysql);
+
+// if($dateJoined) {
+//     if(checkPassword($userName, $password, $dateJoined, $mysql)) {
+//         $tokenPayload = payloadCreate($userName, "client");
+//         echo json_encode(tokenEncode($tokenHeader, $tokenPayload, $SECRETKEY));
+//     };
+// };
+
 ?>
