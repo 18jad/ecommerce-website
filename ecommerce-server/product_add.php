24b9efbf23b7e2a1fc61c7c63248dafe38e0acff
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-21 16:43:27 +0300

    Added Add Products Function

--- ecommerce-server/product_add.php
+++ ecommerce-server/product_add.php
@@ -2,4 +2,34 @@
 
 include("connection.php");
 
+// Init Variables
+
+$sellerUserName = $_POST["userName"];
+$name = $_POST["name"];
+$category = $_POST["category"];
+$description = $_POST["description"];
+$price = $_POST["price"];
+$orders = 0;
+$times_favorited = 0;
+$discount = 0;
+$visited = 0;
+
+// Functions
+
+function addProduct($name, $cat, $desc, $price, $ord, $fav, $disc, $visit, $mysql) {
+    $query = $mysql -> prepare(
+        "INSERT INTO products(`name`, category, `description`,
+        price, orders, times_favorited, discount, visited)
+        VALUE (?, ?, ?, ?, '$ord', '$fav', '$disc', '$visit')");
+
+    if ($query === false) {
+        die(json_encode("error: " . $mysql -> error));
+    };
+
+    $query -> bind_param("ssss", $name, $cat, $desc, $price);
+    $query -> execute();
+
+    return true;
+};
+
 ?>
