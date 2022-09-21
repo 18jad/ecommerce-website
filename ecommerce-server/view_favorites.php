b73014baa54dbd0d03fad5e71c57d2574cd4639d
unknown <aliiobeid01@gmail.com>
2022-09-21 23:14:08 +0300

    include headers,connection

---
+++ ecommerce-server/view_favorites.php
@@ -0,0 +1,13 @@
+<?php
+
+// include the headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+//include the connection to the database
+include("connection.php");
+
+$user_id = $_POST["user_id"];
+
+
+?>
