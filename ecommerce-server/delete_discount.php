077d54a5e80d51cd8ff4168614cd06483000692c
unknown <aliiobeid01@gmail.com>
2022-09-22 00:33:12 +0300

    include headers,connection

---
+++ ecommerce-server/delete_discount.php
@@ -0,0 +1,13 @@
+<?php
+
+// include the headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+
+//include the connection to the database
+include("connection.php");
+
+
+
+?>
