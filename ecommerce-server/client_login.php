3afb670375ae651e8360bc1cede84fb9e4600391
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-21 09:07:12 +0300

    Created Retrieve Date Function

--- ecommerce-server/client_login.php
+++ ecommerce-server/client_login.php
@@ -1,6 +1,31 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
 include("connection.php");
 include("token.php");
+
+// Init Variables
+
+$userName = $_POST["userName"];
+$password = hash("sha256", $_POST["password"] . $dateJoined . "thcaj5445");
+
+// Functions
+
+function retrieveDate() {
+    $query = $mysql -> prepare(
+        "SELECT date_joined AS dj FROM users
+        WHERE username = '$userName'"
+    );
+
+    $query -> execute();
+    $array = $query -> get_result();
+
+    $response = [];
+    $response[] = $array -> fetch_assoc();
+
+    return $response[0]["dj"];
+};
 
 ?>
