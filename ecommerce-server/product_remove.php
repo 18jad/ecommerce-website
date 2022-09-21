34b6d8e96cc2af801f3ae212d4685cd5598e72db
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-21 21:39:13 +0300

    Initialized Variables, Created Function in Remove API

--- ecommerce-server/product_remove.php
+++ ecommerce-server/product_remove.php
@@ -2,4 +2,28 @@
 
 include("connection.php");
 
+// Init Variables
+
+$sellerUserName = $_POST["userName"];
+$productId = $_POST["id"];
+
+// Functions
+
+function deleteProduct($id, $mysql) {
+    $query = $mysql -> prepare(
+        "DELETE FROM products WHERE id = ?"
+    );
+
+    if ($query === false) {
+        die(json_encode("error: " . $mysql -> error));
+    };
+
+    $query -> bind_param("s", $id);
+    $query -> execute();
+
+    return true;
+};
+
+
+
 ?>
