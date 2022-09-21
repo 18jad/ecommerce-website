6fb0ac1810ede7bc633da8a7f9b314e5451615d3
unknown <aliiobeid01@gmail.com>
2022-09-21 15:24:56 +0300

    execute select query and get the results

--- ecommerce-server/clients_data.php
+++ ecommerce-server/clients_data.php
@@ -7,6 +7,16 @@ header("Access-Control-Allow-Headers: *");
 //include the connection to the database
 include("connection.php");
 
+//select info from the table sellers of specific username
+$clients_data = $mysql -> prepare("SELECT * FROM users");
 
+if ($clients_data === false) {
+    die(json_encode("error: " . $mysql -> error));
+};
+
+//execute the select query
+$clients_data -> execute();
+$array = $clients_data -> get_result();
+$response = [];
 
 ?>
