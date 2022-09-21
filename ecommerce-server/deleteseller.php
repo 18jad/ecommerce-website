99a34d612822ff6ebb006ceeebc1f3535084f1a5
unknown <aliiobeid01@gmail.com>
2022-09-21 14:05:29 +0300

    prepare,execute delete query from sellers

--- ecommerce-server/deleteseller.php
+++ ecommerce-server/deleteseller.php
@@ -11,6 +11,17 @@ include("connection.php");
 //declare seller iud varaible to delete on it
 $seller_id=$_POST["seller_id"];
 
+//query to delete seller of specific username
+$delete_seller = $mysql -> prepare("DELETE from sellers where id= $seller_id");
 
+if ($delete_seller === false) {
+    die(json_encode("error: " . $mysql -> error));
+};
+
+//execute the delete query
+$delete_seller -> execute();
+
+// send the resposne with succces message
+echo json_encode("success");
 
 ?>
