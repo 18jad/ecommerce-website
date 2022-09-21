706a115024b922c9b73652405d4d8842e2f2623d
unknown <aliiobeid01@gmail.com>
2022-09-22 00:12:51 +0300

    prepare insert query to the table discounts with fk seller_id

--- ecommerce-server/create_discount.php
+++ ecommerce-server/create_discount.php
@@ -10,5 +10,12 @@ include("connection.php");
 //decalre the post varaibales
 $seller_id= $_POST["seller_id"];
 
+//insert query to the table discounts
+$add_favorite = $mysql -> prepare("INSERT INTO discounts(seller_id,percentage,code) VALUE (?, ?, ?)");
+
+if ($add_favorite=== false) {
+    die(json_encode("error: " . $mysql -> error));
+};
+
 
 ?>
