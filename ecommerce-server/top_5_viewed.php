43c03e19cd1f6873eacfbc7086e1c3cce9c641f4
unknown <aliiobeid01@gmail.com>
2022-09-22 01:52:57 +0300

    prepare select top 5 viewed query from table products

--- ecommerce-server/top_5_viewed.php
+++ ecommerce-server/top_5_viewed.php
@@ -9,5 +9,11 @@ include("connection.php");
 
 $seller_id=$_POST["seller_id"];
 
+//select top 5 visited products
+$top_5= $mysql -> prepare("SELECT * FROM products where seller_id=$seller_id ORDER BY visited DESC LIMIT 5");
+
+if ($top_5 === false) {
+    die(json_encode("error: " . $mysql -> error));
+};
 
 ?>
