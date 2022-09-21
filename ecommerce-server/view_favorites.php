48a6a1823a9ee6d491fa98a3d40f898ce5675109
unknown <aliiobeid01@gmail.com>
2022-09-21 23:21:23 +0300

    fetch the array and get the result

--- ecommerce-server/view_favorites.php
+++ ecommerce-server/view_favorites.php
@@ -23,5 +23,12 @@ $view_favorites -> execute();
 $array = $view_favorites -> get_result();
 $response = [];
 
+//put the data in the response array
+while($info  = $array -> fetch_assoc()){
+    $response[] = $info;
+};
+
+// send the resposne with succces message
+echo json_encode($response);
 
 ?>
