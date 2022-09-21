ad778aea766c7bdd044e5918717f01a9b713db0a
unknown <aliiobeid01@gmail.com>
2022-09-21 03:15:44 +0300

    prepare,execute query and fetch data of specific seller

--- ecommerce-server/editseller.php
+++ ecommerce-server/editseller.php
@@ -15,6 +15,26 @@ $new_name = $_POST["new_name"];
 $new_desc = $_POST["new_desc"];
 $new_money =$_POST["new_money"];
 
+//select info from the table sellers of specific username
+$seller_info = $mysql -> prepare("SELECT name,description,money FROM sellers WHERE username = '$username'");
+
+if ($seller_info === false) {
+    die(json_encode("error: " . $mysql -> error));
+};
+
+//execute the select query
+$seller_info -> execute();
+$array = $seller_info -> get_result();
+$response = [];
+
+//put the data in the response array
+while($info  = $array -> fetch_assoc()){
+    $response[] = $info;
+};
+//fetch the array and get the data
+$name = $response[0]["name"];
+$desc = $response[0]["description"];
+$money = $response[0]["money"];
 
 
 ?>
