d27d243e86f0acf3bfa518788ca37cff69853763
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-21 21:24:01 +0300

    fix carousel

--- client-frontend/script/script.js
+++ client-frontend/script/script.js
@@ -1,3 +1,33 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
 const plusBtnEl = document.querySelector('.plus-btn')
 const minusBtnEl = document.querySelector('.minus-btn')
 const plusMinusInputEl = document.querySelector('.plus-minus-input')
@@ -9,3 +39,5 @@ plusBtnEl.addEventListener("click", () => {
 minusBtnEl.addEventListener("click", () => {
     document.querySelector('.plus-minus-input').value--;
 })  
+
+
