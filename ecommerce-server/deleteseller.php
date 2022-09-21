90b2d0391170f23d2d0540b2351cfdeed98d89bd
unknown <aliiobeid01@gmail.com>
2022-09-21 14:04:20 +0300

    include headers,connection and declare variables

---
+++ ecommerce-server/deleteseller.php
@@ -0,0 +1,16 @@
+<?php
+
+// include the headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+
+//include the connection to the database
+include("connection.php");
+
+//declare seller iud varaible to delete on it
+$seller_id=$_POST["seller_id"];
+
+
+
+?>
