40e88fd1a43f9e86ca599ab247d9f0b246c7553c
unknown <aliiobeid01@gmail.com>
2022-09-21 18:33:16 +0300

    prepare insert query to the table favorites_product

--- ecommerce-server/favorite_add.php
+++ ecommerce-server/favorite_add.php
@@ -11,4 +11,11 @@ include("connection.php");
 $user_id = $_POST["user_id"];
 $product_id = $_POST["product_id"];
 
+//select the username from the database to check if exist
+$add_favorite = $mysql -> prepare("INSERT INTO favorited_products(user_id,product_id) VALUE (?, ?)");
+
+if ($add_favorite=== false) {
+    die(json_encode("error: " . $mysql -> error));
+};
+
 ?>
