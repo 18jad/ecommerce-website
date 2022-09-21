53205b0cc210dbee417a15a413d2e4e817ce6a93
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-21 21:05:30 +0300

    Added Get Data Function

--- ecommerce-server/product_edit.php
+++ ecommerce-server/product_edit.php
@@ -1,6 +1,6 @@
 <?php
 
-// Takes in: userName / name / category / description / price
+// Takes in: userName / id / name / category / description / price
 // Returns true on success. otherwise logs the error
 
 include("connection.php");
@@ -8,6 +8,7 @@ include("connection.php");
 // Init Variables
 
 $sellerUserName = $_POST["userName"];
+$productId = $_POST["id"];
 $name = $_POST["name"];
 $category = $_POST["category"];
 $description = $_POST["description"];
@@ -19,20 +20,42 @@ $visited = 0;
 
 // Functions
 
-function addProduct($id, $name, $cat, $desc, $price, $ord, $fav, $disc, $visit, $mysql) {
+function editProduct($id, $name, $cat, $desc, $price, $ord, $mysql) {
     $query = $mysql -> prepare(
-        "INSERT INTO products(seller_id, `name`, category, `description`,
-        price, orders, times_favorited, discount, visited)
-        VALUE ('$id', ?, ?, ?, ?, '$ord', '$fav', '$disc', '$visit')");
+        "UPDATE products SET `name` = ?, category = ?, `description`= ?,
+        price = ?, orders = ? WHERE id = ?");
 
     if ($query === false) {
         die(json_encode("error: " . $mysql -> error));
     };
 
-    $query -> bind_param("ssss", $name, $cat, $desc, $price);
+    $query -> bind_param("ssssss", $name, $cat, $desc, $price, $ord, $id);
     $query -> execute();
 
     return true;
+};
+
+function getProductData($id, $mysql) {
+    $query = $mysql -> prepare(
+        "SELECT `name`, category, `description`, price
+        FROM products WHERE id = '$id'");
+
+    $query -> execute();
+    $array = $query -> get_result();
+
+    $response = [];
+    
+    while($i = $array -> fetch_assoc()) {
+        $response[] = $i;
+    };
+
+    $data = new stdClass();
+    $data -> name = $response[0]["name"];
+    $data -> category = $response[0]["category"];
+    $data -> description = $response[0]["description"];
+    $data -> price = $response[0]["price"];
+
+    return $data;
 };
 
 function getSellerId($user, $mysql) {
