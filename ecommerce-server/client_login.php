21024a45dddb72e69b5b6ce01616be660b227a52
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-21 10:23:09 +0300

    Added Check Password Function

--- ecommerce-server/client_login.php
+++ ecommerce-server/client_login.php
@@ -4,19 +4,19 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
 include("connection.php");
-include("token.php");
+//include("token.php");
 
 // Init Variables
 
 $userName = $_POST["userName"];
-$password = hash("sha256", $_POST["password"] . $dateJoined . "thcaj5445");
+$password = $_POST["password"];
 
 // Functions
 
-function retrieveDate() {
+function retrieveDate($user, $mysql) {
     $query = $mysql -> prepare(
         "SELECT date_joined AS dj FROM users
-        WHERE username = '$userName'"
+        WHERE username = '$user'"
     );
 
     $query -> execute();
@@ -25,7 +25,42 @@ function retrieveDate() {
     $response = [];
     $response[] = $array -> fetch_assoc();
 
+    if($response[0] == null) {
+        die(json_encode("Username Not Found!"));
+    }
+
     return $response[0]["dj"];
+};
+
+function checkPassword($user, $pass, $date, $mysql) {
+    $hashedPassword = hash("sha256", $pass . $date . "thcaj5445");
+
+    $query = $mysql -> prepare(
+        "SELECT username FROM users
+        WHERE username = '$user' AND password = '$hashedPassword'"
+    );
+
+    $query -> execute();
+    $array = $query -> get_result();
+
+    $response = [];
+    $response[] = $array -> fetch_assoc();
+
+    if($response[0] == null) {
+        die(json_encode("Incorrect Password!"));
+    }
+
+    return true;
+};
+
+// Main
+
+$dateJoined = retrieveDate($userName, $mysql);
+
+if($dateJoined) {
+    if(checkPassword($userName, $password, $dateJoined, $mysql)) {
+        echo "success";
+    };
 };
 
 ?>
