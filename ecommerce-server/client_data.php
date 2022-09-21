f66c6eb14a91f74d4b14125d5a47cc685e39098e
unknown <aliiobeid01@gmail.com>
2022-09-21 15:12:40 +0300

    prepare select query to retrieve sellers data

--- ecommerce-server/client_data.php
+++ ecommerce-server/client_data.php
@@ -7,6 +7,11 @@ header("Access-Control-Allow-Headers: *");
 //include the connection to the database
 include("connection.php");
 
+//select info from the table sellers of specific username
+$seller_data = $mysql -> prepare("SELECT * FROM sellers");
 
+if ($seller_data === false) {
+    die(json_encode("error: " . $mysql -> error));
+};
 
 ?>
