e4c3091f22c1138117d6d270541649648e1d815f
unknown <aliiobeid01@gmail.com>
2022-09-22 01:51:54 +0300

    include headers,connection

---
+++ ecommerce-server/top_5_viewed.php
@@ -0,0 +1,13 @@
+<?php
+
+// include the headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+//include the connection to the database
+include("connection.php");
+
+$seller_id=$_POST["seller_id"];
+
+
+?>
