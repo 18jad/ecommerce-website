64ddd0c6148b108b9843b9a47d5d0102371805fb
unknown <aliiobeid01@gmail.com>
2022-09-22 00:07:55 +0300

    include headers,connection

---
+++ ecommerce-server/create_discount.php
@@ -0,0 +1,14 @@
+<?php
+
+// include the headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+//include the connection to the database
+include("connection.php");
+
+//decalre the post varaibales
+$seller_id= $_POST["seller_id"];
+
+
+?>
