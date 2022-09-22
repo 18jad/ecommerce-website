167d9620b36eda7e24d7dcf81f5edcde4c2b23fa
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-22 23:40:21 +0300

    Created Check Function for Discounts

--- ecommerce-server/discount_validity.php
+++ ecommerce-server/discount_validity.php
@@ -1,8 +1,25 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-
 include("connection.php");
+
+function checkDiscountValidity($id, $code, $mysql) {
+    $query = $mysql -> prepare(
+        "SELECT `percentage` FROM discounts
+        WHERE `code` = ? AND seller_id = ?"
+    );
+
+    $query -> bind_param("ss", $code, $id);
+    $query -> execute();
+    $array = $query -> get_result();
+
+    $response = [];
+    $response[] = $array -> fetch_assoc();
+
+    if($response[0] == null) {
+        return false;
+    };
+
+    return $response;
+};
 
 ?>
