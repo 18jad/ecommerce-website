71d69c46543e09de0d1b94acc14a0c9f04e3c5f3
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-22 20:08:18 +0300

    add eventlistener

--- client-frontend/script/login.js
+++ client-frontend/script/login.js
@@ -1,3 +1,9 @@
 const emailInputEl = document.getElementById('emailInput');
 const passwordInputEl = document.getElementById('passwordInput');
 const authSubmitBtnEl = document.getElementById('authSubmitBtn');
+
+
+authSubmitBtnEl.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log("CLICKED");
+})
