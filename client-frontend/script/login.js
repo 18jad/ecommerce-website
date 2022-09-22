2eb6369dfd60a93b0ab4cf154a8a159cb53bc805
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-22 20:53:48 +0300

    add axios

--- client-frontend/script/login.js
+++ client-frontend/script/login.js
@@ -4,6 +4,26 @@ const authSubmitBtnEl = document.getElementById('authSubmitBtn');
 
 
 authSubmitBtnEl.addEventListener('click', (e) => {
-    e.preventDefault()
-    console.log("CLICKED");
+    e.preventDefault();
+
+    axios({
+        method: "post",
+        url: "http://localhost/ecommerce-server/client_register.php",
+        data: {
+            userName: "Hassan",
+            password: "khalil",
+            email: "hasaan@",
+            name: "Hassan"
+        },
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+        .then(function (response) {
+          //handle success
+          console.log(response);
+        })
+        .catch(function (response) {
+          //handle error
+          console.log(response);
+        });
+
 })
