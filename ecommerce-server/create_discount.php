917e360a5785ab967627a5e53ca9517dfa818f05
unknown <aliiobeid01@gmail.com>
2022-09-22 14:47:38 +0300

    add generate discount code randomly,tested

--- ecommerce-server/create_discount.php
+++ ecommerce-server/create_discount.php
@@ -14,7 +14,11 @@ include("connection.php");
 //decalre the post varaibales
 $seller_id= $_POST["seller_id"];
 $percentage= $_POST["percentage"];
-$code= $_POST["code"];
+
+//decalare length of the discount code
+$length = 6;
+//genarte the code from numbers and letter
+$code= substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'),1,$length);
 
 //insert query to the table discounts
 $add_discount = $mysql -> prepare("INSERT INTO discounts(seller_id,percentage,code) VALUE (?, ?, ?)");
@@ -28,6 +32,6 @@ $add_discount -> bind_param("sss", $seller_id,$percentage,$code);
 $add_discount -> execute();
 
 // send the resposne with succces message
-echo json_encode("discount has been added");
+echo json_encode($code);
 
 ?>
