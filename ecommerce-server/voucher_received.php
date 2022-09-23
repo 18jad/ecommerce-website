f2ca2963fc09fbf4b9a6ac2563ce776df16964dc
unknown <aliiobeid01@gmail.com>
2022-09-23 03:15:11 +0300

    clean code,tested

--- ecommerce-server/voucher_received.php
+++ ecommerce-server/voucher_received.php
@@ -9,18 +9,18 @@ include("connection.php");
 
 $username=$_POST['username'];
 
-
-
 //select qrcode from the table voucher_sent of specific username
 $voucher_received = $mysql -> prepare("SELECT qrcode FROM voucher_sent where user_received='$username'");
 
 if ($voucher_received === false) {
     die(json_encode("error: " . $mysql -> error));
 };
+
 //execute the select query
 $voucher_received -> execute();
 $array =$voucher_received -> get_result();
 $response = [];
+
 //put the data in the response array
 while($info  = $array -> fetch_assoc()){
     $response[] = $info;
