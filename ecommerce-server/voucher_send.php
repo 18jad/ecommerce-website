847b0dcba20fe0f1ec39406b84bb626e8e056fb9
unknown <aliiobeid01@gmail.com>
2022-09-23 03:06:05 +0300

    prepare select query to get the qrcode from table vouhcers

--- ecommerce-server/voucher_send.php
+++ ecommerce-server/voucher_send.php
@@ -11,5 +11,28 @@ $user_send = $_POST["user_send"];
 $username = $_POST["username"];
 $code = $_POST["code"];
 
+//select the qrcode frome table vouchers
+$query = $mysql -> prepare("SELECT qrcode FROM vouchers WHERE code ='$code'");
+
+if ($query=== false) {
+    die(json_encode("error: " . $mysql -> error));
+};
+
+
+//execute the select query
+$query -> execute();
+$array = $query -> get_result();
+$response = [];
+
+//put the data in the array
+while($info  = $array -> fetch_assoc()){
+    $response[] = $info;
+};
+
+
+
+$qr_code = $response[0]["qrcode"];
+
+
 
 ?>
