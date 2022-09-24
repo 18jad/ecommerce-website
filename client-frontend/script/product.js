6400b00550c6bcfb67fd97e8443325b74d1b83cc
charbelmaroundev <charbelmaroun.dev@gmail.com>
2022-09-24 20:36:47 +0300

    add product fill data

--- client-frontend/script/product.js
+++ client-frontend/script/product.js
@@ -2,6 +2,10 @@ const plusBtnEl = document.querySelector(".plus-btn");
 const minusBtnEl = document.querySelector(".minus-btn");
 const plusMinusInputEl = document.querySelector(".plus-minus-input");
 const mySlidesEl = document.querySelector(".mySlides img");
+const rightSectionEl = document.querySelector(".right-section h2");
+const descriptionEl = document.querySelector(".description");
+const rightSectionh3El = document.querySelector(".right-section h3");
+
 let slideIndex = 1;
 
 plusBtnEl.addEventListener("click", () => {
@@ -26,8 +30,11 @@ axios({
 })
   .then(function (response) {
     //handle success
-    console.log(response.data[0]["photo"]);
+    console.log(response.data);
     mySlidesEl.src = response.data[0]["photo"];
+    rightSectionEl.textContent = response.data[0]["name"];
+    descriptionEl.textContent = response.data[0]["description"];
+    rightSectionh3El.textContent = `${response.data[0]["price"]}$`;
   })
   .catch(function (response) {
     //handle error
