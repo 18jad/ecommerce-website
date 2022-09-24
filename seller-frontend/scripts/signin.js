fe6b88ba21701760922bfa512c99d0f4dce0a3af
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2022-09-24 18:18:46 +0300

    edit from email to username

---
+++ seller-frontend/scripts/signin.js
@@ -0,0 +1,9 @@
+const userNameInput = document.getElementById("username");
+const passwordInput = document.getElementById("paswordInput");
+const authSubmitBtnEl = document.getElementById("authSubmitBtn");
+const responseEl = document.getElementById("response");
+
+authSubmitBtnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+
+
