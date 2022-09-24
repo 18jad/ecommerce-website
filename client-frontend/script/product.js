1d6b95996fa34d9a2082dada4240e089ada7b8db
charbelmaroundev <charbelmaroun.dev@gmail.com>
2022-09-24 20:24:14 +0300

    add product image

--- client-frontend/script/product.js
+++ client-frontend/script/product.js
@@ -1,6 +1,7 @@
 const plusBtnEl = document.querySelector(".plus-btn");
 const minusBtnEl = document.querySelector(".minus-btn");
 const plusMinusInputEl = document.querySelector(".plus-minus-input");
+const mySlidesEl = document.querySelector(".mySlides img");
 let slideIndex = 1;
 
 plusBtnEl.addEventListener("click", () => {
@@ -19,13 +20,14 @@ axios({
   method: "POST",
   url: "http://localhost/jacht/product_retrieve.php",
   data: {
-    // prodId: data[i]["id"],
+    prodId: 1,
   },
   headers: { "Content-Type": "multipart/form-data" },
 })
   .then(function (response) {
     //handle success
-    console.log(response);
+    console.log(response.data[0]["photo"]);
+    mySlidesEl.src = response.data[0]["photo"];
   })
   .catch(function (response) {
     //handle error
