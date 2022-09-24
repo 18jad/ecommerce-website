47708de3c8019d1b89e87e4428f8f179910522f4
unknown <aliiobeid01@gmail.com>
2022-09-24 16:36:22 +0300

    add function to get product list of the seller

--- ecommerce-server/seller_statistics.php
+++ ecommerce-server/seller_statistics.php
@@ -10,7 +10,6 @@ $seller_id=$_POST["seller_id"];
 //function to get the top 5 selleviewd productrs depends on the visited
 function getTopViewdProduct($seller_id,$mysql) {
 
-//select top 5 visited products
 $top_5= $mysql -> prepare("SELECT * FROM products where seller_id=$seller_id ORDER BY visited DESC LIMIT 5");
 
 if ($top_5 === false) {
@@ -30,6 +29,27 @@ while($info  = $array -> fetch_assoc()){
 return $response;
 };
 
+//function to get the product lits of the seller
+function getProductList($seller_id,$mysql) {
+
+    $product_list= $mysql -> prepare("SELECT * FROM products where seller_id=$seller_id");
+    
+    if ($product_list === false) {
+        die(json_encode("error: " . $mysql -> error));
+    };
+    
+    //execute the select query
+    $product_list -> execute();
+    $array = $product_list -> get_result();
+    $response = [];
+    
+    //put the data in the response array
+    while($info  = $array -> fetch_assoc()){
+        $response[] = $info;
+    };
+    
+    return $response;
+    };
 
 $TopProduct= getTopViewdProduct($seller_id,$mysql);
 $json = [];
