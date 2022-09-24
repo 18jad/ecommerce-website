fe19658580312b490f61a7f5ca551906e97b2bbb
unknown <aliiobeid01@gmail.com>
2022-09-24 14:49:46 +0300

    commented,tested

--- ecommerce-server/admin_statistics.php
+++ ecommerce-server/admin_statistics.php
@@ -1,10 +1,13 @@
 <?php
 
+//retreive the top 5 clients and top 5 sellers
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
 include("connection.php");
 
+//function to get the top 5 sellers depends on there money
 function getTopSellers($mysql) {
     $query = $mysql -> prepare("SELECT * FROM sellers ORDER BY money DESC LIMIT 5 ");
     
@@ -20,6 +23,7 @@ function getTopSellers($mysql) {
     return $response;
 };
 
+//function to get the top 5 clients depends on there amount_spent
 function getTopClients($mysql) {
     $query = $mysql -> prepare("SELECT * FROM users ORDER BY amount_spent DESC LIMIT 5 ");
     
@@ -35,12 +39,12 @@ function getTopClients($mysql) {
     return $response;
 };
 
+
 $TopSellers = getTopSellers($mysql);
 $Topclients = getTopClients($mysql);
 $json = [];
 $json[] = $TopSellers;
 $json[] = $Topclients;
 echo json_encode($json);
-
 
 ?>
