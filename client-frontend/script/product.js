9cb6fe119669ce30936c701a9aeeda8809f80c8b
charbelmaroundev <charbelmaroun.dev@gmail.com>
2022-09-24 20:13:39 +0300

    remove carousel from product page

--- client-frontend/script/product.js
+++ client-frontend/script/product.js
@@ -14,35 +14,3 @@ minusBtnEl.addEventListener("click", () => {
     document.querySelector(".plus-minus-input").value = 0;
   }
 });
-
-const showSlides = (n) => {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
-};
-
-showSlides(slideIndex);
-
-const plusSlides = (n) => {
-  showSlides((slideIndex += n));
-};
-
-const currentSlide = (n) => {
-  showSlides((slideIndex = n));
-};
