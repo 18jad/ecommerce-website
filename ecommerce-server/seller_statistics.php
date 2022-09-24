8ed01418d8675e9e3c773c46f83dda6412c6e917
unknown <aliiobeid01@gmail.com>
2022-09-24 16:32:02 +0300

    add function to get top 5 viewed products

--- ecommerce-server/seller_statistics.php
+++ ecommerce-server/seller_statistics.php
@@ -7,6 +7,28 @@ include("connection.php");
 
 $seller_id=$_POST["seller_id"];
 
+//function to get the top 5 selleviewd productrs depends on the visited
+function getTopViewdProduct($seller_id,$mysql) {
+
+//select top 5 visited products
+$top_5= $mysql -> prepare("SELECT * FROM products where seller_id=$seller_id ORDER BY visited DESC LIMIT 5");
+
+if ($top_5 === false) {
+    die(json_encode("error: " . $mysql -> error));
+};
+
+//execute the select query
+$top_5 -> execute();
+$array = $top_5 -> get_result();
+$response = [];
+
+//put the data in the response array
+while($info  = $array -> fetch_assoc()){
+    $response[] = $info;
+};
+
+return $response;
+};
 
 
 ?>
