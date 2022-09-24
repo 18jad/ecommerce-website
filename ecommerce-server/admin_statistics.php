5ef1fd4e3fa490665daa019ee598841be12c235c
unknown <aliiobeid01@gmail.com>
2022-09-24 14:45:52 +0300

    add function to get top 5 sellers

--- ecommerce-server/admin_statistics.php
+++ ecommerce-server/admin_statistics.php
@@ -5,6 +5,19 @@ header("Access-Control-Allow-Headers: *");
 
 include("connection.php");
 
+function getTopSellers($mysql) {
+    $query = $mysql -> prepare("SELECT * FROM sellers ORDER BY money DESC LIMIT 5 ");
+    
+    $query -> execute();
+    $array = $query -> get_result();
+    
+    $response = [];
+    
+    while($i = $array -> fetch_assoc()) {
+        $response[] = $i;
+    };
 
+    return $response;
+};
 
 ?>
