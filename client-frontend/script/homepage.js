18bb41e53ca236fcef35165b84685cec005b91ec
charbelmaroundev <charbelmaroun.dev@gmail.com>
2022-09-24 09:40:09 +0300

    add axios fetch authorized

--- client-frontend/script/homepage.js
+++ client-frontend/script/homepage.js
@@ -9,4 +9,24 @@ chatEl.addEventListener("click", () => {
 closeEl.addEventListener("click", () => [chatPopupEl.classList.add("none")]);
 
 let localStorageData = JSON.parse(localStorage.getItem("auth"));
-console.log(localStorageData);
+console.log(localStorageData[1]);
+
+axios({
+  method: "POST",
+  url: "http://localhost/jacht/authorized.php",
+  data: {
+    userName: localStorageData[1],
+    token: localStorageData[2],
+  },
+  headers: { "Content-Type": "multipart/form-data" },
+})
+  .then(function (response) {
+    //handle success
+    if (localStorageData) {
+      console.log(localStorageData);
+    }
+  })
+  .catch(function (response) {
+    //handle error
+    console.log(response);
+  });
