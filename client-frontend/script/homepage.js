0ac887651977b38057e8dded344a144dc133ffc7
charbelmaroundev <charbelmaroun.dev@gmail.com>
2022-09-24 11:40:23 +0300

    fix homepage js

--- client-frontend/script/homepage.js
+++ client-frontend/script/homepage.js
@@ -6,4 +6,32 @@ chatEl.addEventListener("click", () => {
   chatPopupEl.classList.toggle("none");
 });
 
-closeEl.addEventListener("click", () => chatPopupEl.classList.add("none"));
+closeEl.addEventListener("click", () => [chatPopupEl.classList.add("none")]);
+
+let localStorageData = JSON.parse(localStorage.getItem("auth"));
+console.log(localStorageData[1]);
+
+axios({
+  method: "POST",
+  url: "http://localhost/jacht/authorized.php",
+  data: {
+    userName: localStorageData[1],
+    token: localStorageData[2],
+  },
+  headers: { "Content-Type": "multipart/form-data" },
+})
+  .then(function (response) {
+    //handle success
+    if (response.data === true) {
+      fetchingAllData();
+    } else {
+      localStorage.clear();
+      window.location.href = "login.html";
+    }
+  })
+  .catch(function (response) {
+    //handle error
+    console.log(response);
+  });
+
+const fetchingAllData = () => {};
