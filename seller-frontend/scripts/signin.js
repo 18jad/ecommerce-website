19184a84d724d035531a53855abf6f5d6e0d51f3
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2022-09-24 18:19:46 +0300

    adding api url and axios

--- seller-frontend/scripts/signin.js
+++ seller-frontend/scripts/signin.js
@@ -6,4 +6,12 @@ const responseEl = document.getElementById("response");
 authSubmitBtnEl.addEventListener("click", (e) => {
   e.preventDefault();
 
-
+  axios({
+    method: "POST",
+    url: "http://localhost/ecommerce-website/ecommerce-server/seller_login.php",
+    data: {
+      userName: userNameInput.value,
+      password: passwordInput.value,
+    },
+    headers: { "Content-Type": "multipart/form-data" },
+  })
