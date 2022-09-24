4a46b6d44524a84b88cc964af5fc6cebaae47803
charbelmaroundev <charbelmaroun.dev@gmail.com>
2022-09-24 12:03:55 +0300

    add wishlist conditions to open

---
+++ client-frontend/script/wishlist.js
@@ -0,0 +1,24 @@
+let localStorageData = JSON.parse(localStorage.getItem("auth"));
+console.log(localStorageData);
+axios({
+  method: "POST",
+  url: "http://localhost/jacht/authorized.php",
+  data: {
+    userName: localStorageData[1],
+    token: localStorageData[2],
+  },
+  headers: { "Content-Type": "multipart/form-data" },
+})
+  .then(function (response) {
+    //handle success
+    if (response.data === true) {
+      fetchWishlistData(localStorageData[1]);
+    } else {
+      localStorage.clear();
+      window.location.href = "login.html";
+    }
+  })
+  .catch(function (response) {
+    //handle error
+    console.log(response);
+  });
