74f29c11a08baa462d1a1377f3ee3573d16ae5ea
Jad Yahya <jadoyahya@gmail.com>
2022-09-24 21:17:09 +0300

    Admin can now ban clients

---
+++ admin-electron/src/scripts/banClient.js
@@ -0,0 +1,51 @@
+/**
+ * Ban clients:
+ *  - Ban clients by clicking on ban button
+ *  - Ban clients by id
+ */
+
+(() => {
+    setTimeout(() => {
+        const banButtons = document.querySelectorAll('.banBtn'),
+            result = document.getElementById('responseResult');
+
+        const _URL = "http://localhost/ecommerce-website/ecommerce-server/admin_ban_client.php";
+
+        const banClient = (clientUsername) => {
+            axios({
+                method: "POST",
+                url: _URL,
+                data: {
+                    userName: clientUsername,
+                },
+                headers: { "Content-Type": "multipart/form-data" },
+            }).then((response) => {
+                console.log(response)
+                if (response.data) {
+                    result.textContent = "User banned successfully";
+                    result.classList.remove("failed");
+                    result.classList.add("success");
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 2000)
+                } else {
+                    result.textContent = "Error occured please try again";
+                    result.classList.remove("success");
+                    result.classList.add("failed");
+                }
+                result.classList.add("show-result");
+                setTimeout(() => {
+                    result.classList.remove("show-result");
+                }, 2000)
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
+
+        banButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                banClient(button.dataset.username)
+            })
+        })
+    }, 100)
+})();
