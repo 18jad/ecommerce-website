45c359ae6d749e46db93b6cc1a827247b15d3720
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2022-09-24 18:21:16 +0300

    add function response

--- seller-frontend/scripts/signin.js
+++ seller-frontend/scripts/signin.js
@@ -15,3 +15,26 @@ authSubmitBtnEl.addEventListener("click", (e) => {
     },
     headers: { "Content-Type": "multipart/form-data" },
   })
+
+  .then(function (response) {
+    //handle success
+    responseEl.classList.remove("opacity");
+    if (response.data == "Email Not Found!") {
+      responseEl.textContent = response.data;
+      userNameInput.value = "";
+      passwordInput.value = "";
+    } else if (response.data == "Incorrect Password!") {
+      responseEl.textContent = response.data;
+      passwordInput.value = "";
+    } else {
+      responseEl.textContent = "Logged in";
+      const localStorageData = [];
+      localStorageData.push(response.data.id);
+      localStorageData.push(response.data.userName);
+      localStorageData.push(response.data.token);
+      localStorage.setItem("auth", JSON.stringify(localStorageData));
+      window.setTimeout(function () {
+        window.location.href = "index.html";
+      }, 2000);
+    }
+  })
