bf64f8635395f310e69e0c76a8a87d6fbc3b036d
charbelmaroundev <charbelmaroun.dev@gmail.com>
2022-09-24 08:25:05 +0300

    add login response

--- client-frontend/script/login.js
+++ client-frontend/script/login.js
@@ -1,4 +1,4 @@
-const emailInputEl = document.getElementById("emailInput");
+const usernameInputEl = document.getElementById("usernameInput");
 const passwordInputEl = document.getElementById("passwordInput");
 const authSubmitBtnEl = document.getElementById("authSubmitBtn");
 const responseEl = document.getElementById("response");
@@ -10,7 +10,7 @@ authSubmitBtnEl.addEventListener("click", (e) => {
     method: "POST",
     url: "http://localhost/jacht/client_login.php",
     data: {
-      userName: emailInputEl.value,
+      userName: usernameInputEl.value,
       password: passwordInputEl.value,
     },
     headers: { "Content-Type": "multipart/form-data" },
@@ -20,13 +20,25 @@ authSubmitBtnEl.addEventListener("click", (e) => {
       console.log(response.data);
       responseEl.classList.remove("opacity");
 
+      //   if (response.data == "Username Not Found!") {
+      //     responseEl.textContent = response.data;
+      //   } else {
+      //     responseEl.textContent = "Logged in";
+      //     window.setTimeout(function () {
+      //       window.location.href = "homepage.html";
+      //     }, 200000);
+      //   }
+
       if (response.data == "Username Not Found!") {
+        responseEl.textContent = response.data;
+      }
+      if (response.data == "Incorrect Password!") {
         responseEl.textContent = response.data;
       } else {
         responseEl.textContent = "Logged in";
         window.setTimeout(function () {
           window.location.href = "homepage.html";
-        }, 2000);
+        }, 200000);
       }
     })
     .catch(function (response) {
