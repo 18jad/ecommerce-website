76463405fcab484dbfcdf8cb4e4586547a6bf58e
charbelmaroundev <charbelmaroun.dev@gmail.com>
2022-09-24 07:13:02 +0300

    fetch axios register

--- client-frontend/script/register.js
+++ client-frontend/script/register.js
@@ -1,24 +1,29 @@
+const nameInputEl = document.getElementById("nameInput");
+const usernameInputEl = document.getElementById("usernameInput");
 const emailInputEl = document.getElementById("emailInput");
 const passwordInputEl = document.getElementById("passwordInput");
 const authSubmitBtnEl = document.getElementById("authSubmitBtn");
 
+console.log("haha");
 authSubmitBtnEl.addEventListener("click", (e) => {
   e.preventDefault();
 
   axios({
-    method: "post",
-    url: "http://localhost/ecommerce-server/client_login.php",
+    method: "POST",
+    url: "http://localhost/jacht/client_register.php",
     data: {
-      userName: emailInputEl.value,
+      userName: usernameInputEl.value,
+      name: nameInputEl.value,
+      email: emailInputEl.value,
       password: passwordInputEl.value,
-      email: "hasaan@",
-      name: "Hassan",
     },
     headers: { "Content-Type": "multipart/form-data" },
   })
     .then(function (response) {
       //handle success
-      console.log(response);
+      console.log(response.data);
+      if (response === true) {
+      }
     })
     .catch(function (response) {
       //handle error
