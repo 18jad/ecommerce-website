1e88236efd1b562b52fdce0a89993d5840f10cff
unknown <aliiobeid01@gmail.com>
2022-09-25 05:43:19 +0300

    prepare select query to get the data of a user

--- ecommerce-server/client_navbar.php
+++ ecommerce-server/client_navbar.php
@@ -9,6 +9,14 @@ include("image_handler.php");
 
 $user_id=$_POST['user_id'];
 
+//select info from the table users of specific user
+
+$client_data = $mysql -> prepare("SELECT username,name,email,date_joined,money FROM users where id=$user_id");
+
+if ($client_data === false) {
+    die(json_encode("error: " . $mysql -> error));
+};
+
 
 
 ?>
