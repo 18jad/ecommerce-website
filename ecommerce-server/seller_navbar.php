98fa3a86aafad45d656644daf585f1e422487a65
unknown <aliiobeid01@gmail.com>
2022-09-25 05:52:04 +0300

    prepare select query to get the data of a seller

--- ecommerce-server/seller_navbar.php
+++ ecommerce-server/seller_navbar.php
@@ -12,5 +12,13 @@ include("connection.php");
 
 $seller_id=$_POST['seller_id'];
 
+//select info from the table users of specific user
+
+$seller_data = $mysql -> prepare("SELECT username,name,description,date_joined,money FROM sellers where id=$seller_id");
+
+if ($seller_data === false) {
+    die(json_encode("error: " . $mysql -> error));
+};
+
 
 ?>
