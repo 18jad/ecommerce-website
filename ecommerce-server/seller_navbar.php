ae047bb93845ea47ba2a4716b76a0985adf287a8
unknown <aliiobeid01@gmail.com>
2022-09-25 05:51:33 +0300

    include headers and connection and post variables

---
+++ ecommerce-server/seller_navbar.php
@@ -0,0 +1,16 @@
+<?php
+
+//takes user id
+//retrieve data of the user
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+//include the connection to the database
+include("connection.php");
+
+
+$seller_id=$_POST['seller_id'];
+
+
+?>
