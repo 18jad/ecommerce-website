4504b4571ddff701d73eb68896abda20526f467e
charbelmaroundev <charbelmaroun.dev@gmail.com>
2022-09-25 14:59:05 +0300

    add homepage amount user

--- client-frontend/script/homepage.js
+++ client-frontend/script/homepage.js
@@ -1,6 +1,8 @@
 const chatEl = document.querySelector(".chat");
 const chatPopupEl = document.querySelector(".chat-popup");
 const closeEl = document.querySelector(".close");
+const userAmountEl = document.querySelector(".user-amount");
+const searchDataEl = document.querySelector(".search-data");
 
 chatEl.addEventListener("click", () => {
   chatPopupEl.classList.toggle("none");
@@ -9,7 +11,7 @@ chatEl.addEventListener("click", () => {
 closeEl.addEventListener("click", () => [chatPopupEl.classList.add("none")]);
 
 let localStorageData = JSON.parse(localStorage.getItem("auth"));
-console.log(localStorageData[1]);
+// console.log(localStorageData[1]);
 
 axios({
   method: "POST",
@@ -34,4 +36,24 @@ axios({
     console.log(response);
   });
 
-const fetchingAllData = () => {};
+const fetchingAllData = () => {
+  // console.log(localStorageData[0]);
+
+  axios({
+    method: "POST",
+    url: "http://localhost/jacht/client_navbar.php",
+    data: {
+      user_id: localStorageData[0],
+    },
+    headers: { "Content-Type": "multipart/form-data" },
+  })
+    .then(function (response) {
+      //handle success
+      // console.log(response.data);
+      userAmountEl.textContent = `${response.data[0]["money"]}$`;
+    })
+    .catch(function (response) {
+      //handle error
+      console.log(response);
+    });
+};
