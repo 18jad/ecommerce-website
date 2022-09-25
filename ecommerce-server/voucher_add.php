68407c781ee23f9493a2b2bf360441c881dec959
unknown <aliiobeid01@gmail.com>
2022-09-25 16:40:37 +0300

    include headers,connection and declare post variables

---
+++ ecommerce-server/voucher_add.php
@@ -0,0 +1,16 @@
+<?php
+
+// include the headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+//include the connection to the database
+include("connection.php");
+
+//decalre the post varaibales
+$user_id= $_POST["user_id"];
+$code = $_POST["code"];
+
+
+
+?>
