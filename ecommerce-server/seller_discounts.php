4a86dc91da9f1782c03ccc05c3fae17f41a5c72b
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-25 21:06:41 +0300

    Created New Seller Discounts API

---
+++ ecommerce-server/seller_discounts.php
@@ -0,0 +1,49 @@
+<?php
+
+// Takes in: sellerId.
+// Returns: All Products of Seller.
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+include("connection.php");
+
+// Init Variables
+
+$sellerId = $_POST["sellerId"];
+
+// Functions
+
+function getData($id, $mysql) {
+    $query = $mysql -> prepare(
+        "SELECT * FROM discounts
+        WHERE seller_id = ?"
+    );
+
+    $query -> bind_param("s", $id);
+    $query -> execute();
+    $array = $query -> get_result();
+
+    $response = [];
+    while($i = $array -> fetch_assoc()) {
+        $response[] = $i;
+    };
+
+    return $response;
+};
+
+// Main
+
+$response = getData($sellerId, $mysql);
+$i = 0;
+
+foreach($response as $res) {
+    $prodId = $res["id"];
+    $image = imageRetrieve($prodId, "product", $mysql);
+    $response[$i]["photo"] = $image;
+    $i++;
+};
+
+echo json_encode($response);
+
+?>
