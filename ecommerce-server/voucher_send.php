f2389eaef7a9e78db0a39188608d30ea6b6a04a6
unknown <aliiobeid01@gmail.com>
2022-09-25 17:59:29 +0300

    prepare,execute select query from users on username

--- ecommerce-server/voucher_send.php
+++ ecommerce-server/voucher_send.php
@@ -11,6 +11,22 @@ include("connection.php");
 $username= $_POST["username"];
 $user_id= $_POST["user_id"];
 
+$user_id_send = $mysql -> prepare("SELECT id FROM users WHERE username='$username'");
+
+//execute the select query
+$user_id_send -> execute();
+$array = $user_id_send -> get_result();
+$get_userid = [];
+
+//put the data in the array
+while($info  = $array -> fetch_assoc()){
+    $response[] = $info;
+};
+
+//get the id of the user send
+$json=json_encode($response);
+$user_send=$response[0]['id'];
+
 
 
 ?>
