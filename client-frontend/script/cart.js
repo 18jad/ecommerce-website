262be664a4bc907b29e0749326a33d5b66abdda0
charbelmaroundev <charbelmaroun.dev@gmail.com>
2022-09-25 10:43:40 +0300

    add cart eventlistener for remove

--- client-frontend/script/cart.js
+++ client-frontend/script/cart.js
@@ -41,24 +41,53 @@ const shoppingCartFetch = () => {
       headers: { "Content-Type": "multipart/form-data" },
     })
       .then(function (response) {
+        // console.log(response.data[0]["id"]);
         //handle success
         let table = `
-           <tr>
+          <tr>
             <td><img class="cart-image" src="${
               response.data[0]["photo"]
-            }" alt=""></td>
+            }" alt="product image"></td>
             <td>${response.data[0]["name"]}</td>
             <td>${response.data[0]["price"]}</td>
             <td>${quantity[i]}</td>
             <td>${quantity[i] * response.data[0]["price"]}</td>
-            <td><i class="fa-solid fa-xmark"></i></td>
-        </tr>`;
+            <td class="close-icon" id ="${response.data[0]["id"]}" >
+              <svg class="close-icon" id ="${
+                response.data[0]["id"]
+              }" fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px">
+                <path
+                d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z" />
+              </svg>
+            </td>
+          </tr>`;
 
         tableEl.insertAdjacentHTML("afterend", table);
+        // const removesvgEl = document.querySelector("svg");
+        const removebtnEl = document.querySelector("svg");
+        // console.log(removebtnEl);
+
+        removebtnEl.addEventListener("click", (removebtn) => {
+          console.log(removebtn.path[1]["id"]);
+
+          
+        });
       })
       .catch(function (response) {
         //handle error
         console.log(response);
       });
   }
+  // removecartClickedBtn();
 };
+
+// const removecartClickedBtn = () => {
+//   const removebtnEl = document.querySelector(".close-icon svg");
+//   console.log(removebtnEl);
+
+//   removebtnEl.forEach((removebtn) => {
+//     removebtn.addEventListener("click", (removebtn) => {
+//       console.log(removebtn.path[0].id);
+//     });
+//   });
+// };
