049b226ad980225361f3073d4fa841eacdc0cd92
unknown <aliiobeid01@gmail.com>
2022-09-25 17:57:42 +0300

    include headers,connection and declare post variables

--- ecommerce-server/voucher_send.php
+++ ecommerce-server/voucher_send.php
@@ -8,41 +8,9 @@ header("Access-Control-Allow-Headers: *");
 include("connection.php");
 
 //decalre the post varaibales
-$user_send = $_POST["user_send"];
-$username = $_POST["username"];
-$code = $_POST["code"];
+$username= $_POST["username"];
+$user_id= $_POST["user_id"];
 
-//select the qrcode frome table vouchers
-$query = $mysql -> prepare("SELECT qrcode FROM vouchers WHERE code ='$code'");
 
-if ($query=== false) {
-    die(json_encode("error: " . $mysql -> error));
-};
-
-//execute the select query
-$query -> execute();
-$array = $query -> get_result();
-$response = [];
-
-//put the data in the array
-while($info  = $array -> fetch_assoc()){
-    $response[] = $info;
-};
-
-$qr_code = $response[0]["qrcode"];
-
-//insert into table voucher_sent
-$send_voucher = $mysql -> prepare("INSERT INTO voucher_sent(user_send,user_received,qrcode) VALUE (?, ?, ?)");
-
-if ($send_voucher === false) {
-    die(json_encode("error: " . $mysql -> error));
-};
-
-//execute the insert query
-$send_voucher -> bind_param("sss",$user_send,$username,$qr_code);
-$send_voucher -> execute();
-
-// send the resposne with succces message
-echo json_encode("success");
 
 ?>
